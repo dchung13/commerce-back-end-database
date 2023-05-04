4e57af6e01d2b4a26e516229b0ed744a35fba3be
David C <dchung20315@gmail.com>
2023-05-03 22:08:03 -0700

    changed small things

--- models/index.js
+++ models/index.js
@@ -11,20 +11,17 @@ Product.belongsTo(Category, {
 
 // Categories have many Products
 Category.hasMany(Product, {
-  foreignKey: 'category_id',
-  onDelete: 'CASCADE',
+  foreignKey: 'category_id'
 });
 
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
-  through: ProductTag,
-  foreignKey: 'product_id'
+  through: ProductTag
 });
 
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
-  through: ProductTag,
-  foreignKey: 'tag_id'
+  through: ProductTag
 });
 
 module.exports = {
